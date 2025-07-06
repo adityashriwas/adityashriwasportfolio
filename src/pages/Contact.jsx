4e9301e57fb1a23dbd3ff66822ace4f07b850a80
Aditya Shriwas <adityashriwas09@gmail.com>
2025-07-06 17:09:06 +0530

    first commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
           from_name: form.name,
           to_name: "Aditya Shriwas",
           from_email: form.email,
-          to_email: "adityashriwas09@gmail.com",
+          to_email: "adityashriwas08@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
